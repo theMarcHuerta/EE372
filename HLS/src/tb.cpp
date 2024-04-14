26ee34866c0d8b1a792e66702bfaaee297114d3c
Brian Christopher Kaether <bkaether@caddy10.stanford.edu>
2024-04-14 01:05:19 -0700

    example which indicates done and makes it through rtl synthesis

--- HLS/src/tb.cpp
+++ HLS/src/tb.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
+
 #include "renderer.h"
+#include "vec3.h"
+
 using namespace std;
 
 CCS_MAIN(int argc, char** argv) {
     
-    Renderer inst; // DUT
+    Renderer<32, 8, true> inst; // DUT
 
     vec3<32, 8, true> in(-2.5, -2.5, -2.5); // input
     vec3<32, 8, true> out; // output
 
     inst.run(in, out);
+
+    cout << "Done!\n" << endl;
 
     CCS_RETURN(0);
 }
