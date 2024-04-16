9c1640d37cb45c0fa2729f7178a2fe3a38b5a282
Brian Christopher Kaether <bkaether@caddy10.stanford.edu>
2024-04-16 00:52:02 -0700

    added pragma for at function

--- HLS/src/ray.h
+++ HLS/src/ray.h
@@ -16,6 +16,9 @@ class ray {
     ray() {}
     ray(vec3<T> origin, vec3<T> direction) : orig(origin), dir(direction) {}
 
+    // Calculates a point along the ray at parameter t.
+    #pragma hls_design ccore
+    #pragma hls_ccore_type combinational
     vec3<T> at(T& t) {
         vec3<T> mult_result = dir.mult(t);
         return orig.add(mult_result);
