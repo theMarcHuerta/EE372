73f85650f399ea55c868493ca3892e79b8e8656b
Brian Christopher Kaether <bkaether@caddy10.stanford.edu>
2024-04-16 18:18:14 -0700

    fixed type errors in rtcore

--- HLS/src/RTcore.h
+++ HLS/src/RTcore.h
@@ -30,89 +30,85 @@ typedef ac_int<64, false> uint_64;
 
 #include "vec3.h"
 
-// struct vec3_fp_11_22 { // vector point-- can be as big as the world space
-//   int_11 x_i;
-//   int_11 x_f1; // first half of fixed point
-//   int_11 x_f2; // second half
-//   int_11 y_i;
-//   int_11 y_f1;
-//   int_11 y_f2;
-//   int_11 z_i;
-//   int_11 z_f1;
-//   int_11 z_f2;
-// };
+struct vec3_fp_11_22 { // vector point-- can be as big as the world space
+  int_11 x_i;
+  int_11 x_f1; // first half of fixed point
+  int_11 x_f2; // second half
+  int_11 y_i;
+  int_11 y_f1;
+  int_11 y_f2;
+  int_11 z_i;
+  int_11 z_f1;
+  int_11 z_f2;
+};
 
-// struct pxl_deltas { 
-//   sfp_3_22 pixel_delta_u;
-//   sfp_3_22 pixel_delta_v
-// };
+template <typename T>
+struct pxl_deltas { 
+  vec3<T> pixel_delta_u;
+  vec3<T> pixel_delta_v;
+};
 
-// struct rgb_t { // standard sdr
-//   uint_8 r;
-//   uint_8 g;
-//   uint_8 b;
-// };
+struct rgb_t { // standard sdr
+  uint_8 r;
+  uint_8 g;
+  uint_8 b;
+};
 
-// struct ray {
-//   ws_point_full_fp origin; 
-//   full_vec3 ray_direction;
-// }
+struct ray {
+  vec3<sfp_11_22> origin; 
+  vec3<sfp_11_22> ray_direction;
+};
 
-// struct img_params {
-//   // might not even need these
-// //   uint_2 aspect_ratio; // 
-//   int_11          num_spheres;
-//   int_11          num_quads;
-//   uint_2          samp_per_pxl; // 64-128-256-512
-// //   uint_2 vfov; // 20,40,60,80
-//   rgb_t           background;
-// //   ws_point lookfrom;
-// //   ws_point lookat;
-//   // derrivied values (might just pass these in)
-//   uint_10         image_height;   // Height of the image, computed from width and aspect ratio.
-//   uint_10         image_width;   // Height of the image, computed from width and aspect ratio.
-//   ws_point        center;         // The position of the camera (same as lookfrom).
-//   vec3_fp_11_22   pixel00_loc;    // Location in space of the top-left pixel.
-//   vec3_fp_11_22   pixel_delta_u;  // Vector to move one pixel to the right on the image plane.
-//   vec3_fp_11_22   pixel_delta_v;  // Vector to move one pixel down on the image plane.
-// //   cam_params(uint_2 a, uint_2 b, uint_2 c) {
-// //     aspect_ratio=a;
-// //     samp_per_pxl=b;
-// //   }
-// };
+struct img_params {
+  // might not even need these
+//   uint_2 aspect_ratio; // 
+  int_11          num_spheres;
+  int_11          num_quads;
+  uint_2          samp_per_pxl; // 64-128-256-512
+//   uint_2 vfov; // 20,40,60,80
+  rgb_t           background;
+//   vec3<int_11> lookfrom;
+//   vec3<int_11> lookat;
+  // derrivied values (might just pass these in)
+  uint_10         image_height;   // Height of the image, computed from width and aspect ratio.
+  uint_10         image_width;   // Height of the image, computed from width and aspect ratio.
+  vec3<int_11>        center;         // The position of the camera (same as lookfrom).
+  vec3_fp_11_22   pixel00_loc;    // Location in space of the top-left pixel.
+  vec3_fp_11_22   pixel_delta_u;  // Vector to move one pixel to the right on the image plane.
+  vec3_fp_11_22   pixel_delta_v;  // Vector to move one pixel down on the image plane.
+//   cam_params(uint_2 a, uint_2 b, uint_2 c) {
+//     aspect_ratio=a;
+//     samp_per_pxl=b;
+//   }
+};
 
-// struct LoopIndices{
-//     uint_11 x_pxl;
-//     uint_11 y_pxl;
-// };
+struct LoopIndices{
+    uint_11 x_pxl;
+    uint_11 y_pxl;
+};
 
 
-// struct pxl_params {
-//   ws_point        center;
-//   vec3_fp_11_22   pixel00_loc;    // Location in space of the top-left pixel.
-// };
+struct pxl_params {
+  vec3<int_11>        center;
+  vec3_fp_11_22   pixel00_loc;    // Location in space of the top-left pixel.
+};
 
-// // struct pxl_deltas {
-// //   vec3_fp_11_22   pixel_delta_u;  // Vector to move one pixel to the right on the image plane.
-// //   vec3_fp_11_22   pixel_delta_v;  // Vector to move one pixel down on the image plane.
-// // }
+struct sphere_hittable {
+    vec3<int_11> center; // for quads its corner, sphere it's center
+    uint_8 radius; // radius cant clip edge of range of ws view
+    uint_2 mat_type; // allows for 4 possible materials, light, lambertian, metallic/specular, diaelectric??
+};
 
-// struct sphere_hittable {
-//     ws_point center; // for quads its corner, sphere it's center
-//     uint_8 radius; // radius cant clip edge of range of ws view
-//     uint_2 mat_type; // allows for 4 possible materials, light, lambertian, metallic/specular, diaelectric??
-// }
-
-// struct quad_hittable {
-//     ws_point corner_pt; // for quads its corner
-//     vec3 u; // defining u component 
-//     vec3 v; // defining v componenet
-//     uint_2 mat_type; // allows for 4 possible materials, light, lambertian, metallic/specular, diaelectric??
-//     pbool is_invis;
-//     vec3 normal; // cross of u and v
-//     vec3 w; // dot of u and v
-//     int_12 d_plane;
-// }
+struct quad_hittable {
+    vec3<int_11> corner_pt; // for quads its corner
+    vec3<int_11> u; // defining u component 
+    vec3<int_11> v; // defining v componenet
+    uint_2 mat_type; // allows for 4 possible materials, light, lambertian, metallic/specular, diaelectric??
+    pbool is_invis;
+    vec3<int_11> normal; // cross of u and v
+    vec3<int_11> w; // dot of u and v
+    int_12 d_plane;
+};
 
 
 #define WS_MAX_X 1023
