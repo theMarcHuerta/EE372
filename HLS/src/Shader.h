3574f72bdcc6dcbb4ce1f3d97f1dff4466b553b4
themarchuerta <huertamarc68@gmail.com>
2024-04-24 01:36:10 -0700

    fixed err

--- HLS/src/Shader.h
+++ HLS/src/Shader.h
@@ -26,8 +26,8 @@ public:
     {   
 
         worldHit.hit(ray_in, params_in, sphere_in, quad_in, attenuation_chan_in, accumalated_color_chan_in, 
-                     attenuation_chan_through, accumalated_color_through, worldRec, rayToScatter, isWorldHit, paramsThrough);
-        materialScatter.scatter(rayToScatter, worldRec, paramsThrough, attenuation_chan_through, accumalated_color_through, isWorldHit,
+                     attenuation_chan_through, accumalated_color_through, worldRec, rayToScatter, isWorldHit);
+        materialScatter.scatter(rayToScatter, worldRec, attenuation_chan_through, accumalated_color_through, isWorldHit,
                                  attenuation_chan_out, accumalated_color_out, ray_out);
 
     }
@@ -40,7 +40,6 @@ private:
     ac_channel<rgb_in> accumalated_color_through;
     ac_channel<bool> isWorldHit;
     ac_channel<HitRecord<T>> worldRec;
-    ac_channel<img_params> paramsThrough;
 };
 
 #endif
