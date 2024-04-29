be5c15d94952a6c352532b97ed145495d9755f47
themarchuerta <huertamarc68@gmail.com>
2024-04-29 06:57:00 -0700

    fixed a bunch more stuff, rand is a work in a progress

--- HLS/src/LambertianScatter.h
+++ HLS/src/LambertianScatter.h
@@ -2,29 +2,32 @@
 
 #include "RTcore.h"
 
-template<typename T>
+
 class LambertianScatter {
   public:
     LambertianScatter() {}
 
     #pragma hls_design
-    void run(HitRecord<T>& rec, ray<T>& scattered) {
-        vec3<T> rand_dir;
-        vec3<T> scatter_direction;
+    void run(HitRecord& rec, ray& scattered) {
+        vec3<ac_fixed<34, 2, true>> rand_dir;
+        vec3<ac_fixed<34, 3, true>> scatter_direction;
 
         rand_unit.run(rand_dir);
         add.run(rec.normal, rand_dir, scatter_direction);
 
-        if ((scatter_direction.x == 0) && (scatter_direction.y == 0) && (scatter_direction.z == 0)) {
+        vec3<ac_fixed<34, 2, true>> near_zero_val = 1.49011612e-8;
+        if ((scatter_direction.x < near_zero_val) && (scatter_direction.y < near_zero_val) && (scatter_direction.z < near_zero_val)) {
             scatter_direction = rec.normal;
         }
 
+        // LETS SEE IF IT TRUNCATES TO RIGHT SIZE OTHERWISE WE HAVE TO MANUALLY SET IT
         scattered = {{rec.hit_loc.x, rec.hit_loc.y, rec.hit_loc.z},
                          {scatter_direction.x, scatter_direction.y, scatter_direction.z}};
 
     }
 
   private:
-    Vec3_add<T> add;
-    Rand_unit_vec<T> rand_unit;
+  // rec normal size, rand dir size, resulting size
+    Vec3_add<ac_fixed<26, 2, true>, ac_fixed<34, 2, true>, ac_fixed<34, 3, true>> add;
+    Rand_unit_vec rand_unit;
 };
--- HLS/src/Rand.h
+++ HLS/src/Rand.h
@@ -4,46 +4,95 @@
 #include "RTcore.h"
 #include "vec3.h"
 
-template<typename T>
 class Rand_val {
   public:
     Rand_val(int seed = 375821) : state(seed) {}
-
     // Generates a random fixed point value between 0 and 1
-    void run(T& out) {
-      ac_int<22, false> feedback = ((state >> 22) ^ (state >> 17) ^ (state >> 3) ^ (state >> 2)) & 1;
-      state = ((state << 1) | feedback);
-      out = 0;
-      out.set_slc(0, state);
+    void run(ac_int<33, 1, false>& out) {
+      ac_int<32, false> x = state;
+      x ^= x << 13;
+      x ^= x >> 17;
+      x ^= x << 5;
+      state = x;
+      // set lower 32 bits of ac fixed out
+      out.set_slc(0, x.slc<32>(0));
     }
 
   private:
-    ac_int<22, false> state;
+    ac_int<32, false> state;
 };
 
-template<typename T>
-class Rand_vec {
-  Rand_val<T> rand;
-  public:
-    // Generates a random vec3
-    void run(vec3<T>& out) {
-        rand.run(out.x);
-        rand.run(out.y);
-        rand.run(out.z);
+class random_in_unit_sphere {
+    Rand_val random1;
+    Rand_val random2;
+    public:
+    voic run(){
+
+      Rand_val.run(xi1);
+      Rand_val.run(xi2);
+
+      ax_fixed theta = 2.0 * 3.1415926 * xi1; // Uniform distribution from 0 to 2π
+      ax_fixed phi = std::acos(2.0 * xi2 - 1.0); // Correctly distribute from -1 to 1 and acos
+
+      ax_fixed xs = std::sin(phi) * std::cos(theta); // Note the switch of phi and theta here
+      ax_fixed ys = std::sin(phi) * std::sin(theta);
+      ax_fixed zs = std::cos(phi);
+
+      out = vec3(xs, ys, zs);
     }
-};
+}
 
-template<typename T>
+
 class Rand_unit_vec {
-  Rand_vec<T> rand_vec;
-  Vec3_unit<T> unit;
+  random_in_unit_sphere rand_iuv;
+  Vec3_unit<ac_fixed<34, 2, true>, ac_fixed<34, 2, true>> unit;
   public:
     // Generates a random unit vector. Useful in simulating diffuse reflections.
     void run(vec3<T>& out) {
-        vec3<T> v;
-        rand_vec.run(v);
-        unit.run(v, out);
+      rand_iuv.run();
+      unit.run();
     }
 };
+
+// class Rand_val {
+//   public:
+//     Rand_val(int seed = 375821) : state(seed) {}
+
+//     // Generates a random fixed point value between 0 and 1
+//     void run(T& out) {
+//       ac_int<22, false> feedback = ((state >> 22) ^ (state >> 17) ^ (state >> 3) ^ (state >> 2)) & 1;
+//       state = ((state << 1) | feedback);
+//       out = 0;
+//       out.set_slc(0, state);
+//     }
+
+//   private:
+//     ac_int<22, false> state;
+// };
+
+// template<typename T>
+// class Rand_vec {
+//   Rand_val<T> rand;
+//   public:
+//     // Generates a random vec3
+//     void run(vec3<T>& out) {
+//         rand.run(out.x);
+//         rand.run(out.y);
+//         rand.run(out.z);
+//     }
+// };
+
+
+// class Rand_unit_vec {
+//   Rand_vec<ac_fixed<34, 2, true>> rand_vec;
+//   Vec3_unit<ac_fixed<34, 2, true>> unit;
+//   public:
+//     // Generates a random unit vector. Useful in simulating diffuse reflections.
+//     void run(vec3<T>& out) {
+//         vec3<T> v;
+//         rand_vec.run(v);
+//         unit.run(v, out);
+//     }
+// };
 
 #endif
