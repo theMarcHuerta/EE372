772e3aeb8323113425619ec1b83954388bd9576c
theMarcHuerta <huertamarc68@gmail.com>
2024-04-29 22:32:58 -0700

    fixes

--- HLS/src/ray.h
+++ HLS/src/ray.h
@@ -6,17 +6,15 @@
 
 template<typename T1, typename T2>
 class Ray_at {
-  
-  
   public:
     // Calculates a point along the ray at parameter t.
     #pragma hls_design ccore
     #pragma hls_ccore_type combinational
     void run(ray& r, T1& t, vec3<T2>& out) {
-        Vec3_mult_s<ac_fixed<34, 11, true>, T1, ac_fixed<34+t.wdith, 11+t.i_width, true>> mult;
-        Vec3_add<ac_fixed<21, 11, true>, ac_fixed<34+t.wdith, 11+t.i_width, true>, T2> add;
+        Vec3_mult_s<ac_fixed<34, 11, true>, T1, ac_fixed<34+t.width, 11+t.i_width, true>> mult;
+        Vec3_add<ac_fixed<21, 11, true>, ac_fixed<34+t.width, 11+t.i_width, true>, T2> add;
 
-        vec3<ac_fixed<34+t.wdith, 11+t.i_width, true>> mult_result; // sized to be result of ray dir and width of input
+        vec3<ac_fixed<34+t.width, 11+t.i_width, true>> mult_result; // sized to be result of ray dir and width of input
         mult.run(r.dir, t, mult_result);
         add.run(r.orig, mult_result, out); // sized for ray origin// outshould be dir + t2 width + 1
     }
