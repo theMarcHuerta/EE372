3453de560b48c3045dc10a88947149cebce687a5
Brian Christopher Kaether <bkaether@caddy10.stanford.edu>
2024-05-06 22:44:02 -0700

    more script and makefile modifications

--- HLS/scripts/QuadBuffer.tcl
+++ HLS/scripts/QuadBuffer.tcl
@@ -3,21 +3,15 @@ set blockname [file rootname [file tail [info script] ]]
 source scripts/common_no_mod.tcl
 
 directive set -DESIGN_HIERARCHY " 
-    {ShaderCores} 
+    {QuadBuffer} 
 "
 
 go compile
 
 source scripts/set_libraries.tcl
 
-solution library add "\[Block\] Shader.v1"
-
 go libraries
 directive set -CLOCKS $clocks
-
-directive set /ShaderCores/Shader -MAP_TO_MODULE "\[Block\] Shader.v1"
-
-directive set /ShaderCores -FIFO_DEPTH 3
 
 go assembly
 directive set -EFFORT_LEVEL high
--- HLS/scripts/Renderer.tcl
+++ HLS/scripts/Renderer.tcl
@@ -16,6 +16,11 @@ solution library add "\[Block\] RayGeneration.v1"
 go libraries
 directive set -CLOCKS $clocks
 
+directive set /RendererWrapper/ShaderCores -MAP_TO_MODULE "\[Block\] ShaderCores.v1"
+directive set /RendererWrapper/RayGeneration -MAP_TO_MODULE "\[Block\] RayGeneration.v1"
+
+directive set /RendererWrapper -FIFO_DEPTH 3
+
 go assembly
 directive set -EFFORT_LEVEL high
 
