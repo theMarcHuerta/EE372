4e3fe75bc331424aba88248b98b60e93905f885f
Brian Christopher Kaether <bkaether@caddy10.stanford.edu>
2024-05-11 19:19:05 -0700

    updated all scripts to use 9ns period for now

--- HLS/scripts/common_no_mod.tcl
+++ HLS/scripts/common_no_mod.tcl
@@ -34,11 +34,11 @@ catch {
     solution remove -solution $existing_solution -delete
 }
 
-set clk_period 17.0
+set clk_period 9.0
 set clocks "clk \"-CLOCK_PERIOD $clk_period -CLOCK_EDGE rising -CLOCK_HIGH_TIME [expr $clk_period/2] -CLOCK_OFFSET 0.000000 -CLOCK_UNCERTAINTY 0.0 -RESET_KIND async -RESET_SYNC_NAME rst -RESET_SYNC_ACTIVE high -RESET_ASYNC_NAME arst_n -RESET_ASYNC_ACTIVE low -ENABLE_NAME {} -ENABLE_ACTIVE high\" "
 
 go new
 solution file add ./src/Pathtracer.cpp
-# solution file add ./testbenches/PathtracerTb.cpp -exclude true
+solution file add ./testbenches/PathtracerTb.cpp -exclude true
 
 go analyze
